2f7ab375621cc7ee8e4d26f986e2c7e8dd893c7b
Anthony Galczak <WGalczak@gmail.com>
2016-11-06 18:51:05 -0700

    beginning lab 7

---
+++ UNM/CS241L/lab7/binarytree.c
@@ -0,0 +1,77 @@
+/********************************/
+/* Anthony Galczak              */
+/* 11-5-16                      */
+/* CS-241 Section 001           */
+/* Lab 7                        */
+/* Working with Binary Search   */
+/* Trees and Linked Lists       */
+/********************************/
+
+
+#include <stdio.h>
+#include "binarytree.h"
+
+
+struct TreeNode
+{
+  int data;
+  struct TreeNode* left
+  struct TreeNode* rigt;
+}
+
+
+struct TreeNode* createNode(int data)
+{
+    
+}
+
+
+int removeBST(struct TreeNode** rootRef, int data)
+{
+  
+}
+
+
+int maxValueBST(struct TreeNode* root)
+{
+  
+}
+
+
+int maxDepth(struct TreeNode* root)
+{
+  
+}
+
+
+int isBalanced(struct TreeNode* root)
+{
+  
+}
+
+
+int isBST(struct TreeNode* root)
+{
+  
+}
+
+
+void printTree(struct TreeNode* root)
+{
+  
+}
+
+
+void printLeaves(struct TreeNode* root)
+{
+  
+}
+
+
+void freeTree(struct TreeNode* root)
+{
+  
+}
+
+
+
