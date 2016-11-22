36563d29a2a6ffad786a8af11d8d0152e65e4a22
Anthony Galczak <WGalczak@gmail.com>
2016-11-22 13:09:42 -0700

    cs152 lab9

---
+++ UNM/CS152L/Lab 9/Card.java
@@ -0,0 +1,54 @@
+// Anthony Galczak - agalczak@unm.edu
+// Program 9 - Card and Deck classes
+// These classes will allow us to play a swing GUI solitaire game
+
+public class Card {
+	
+	private Rank rank;
+	private Suit suit;
+	private boolean faceUp;
+	private boolean isBlack;
+
+	public Card(Rank rank, Suit suit)
+	{
+		this.rank = rank;
+		this.suit = suit;
+		if(suit.name() == "CLUBS" || suit.name() == "SPADES")
+		{
+			isBlack = true;
+		}
+		else
+		{
+			isBlack = false;
+		}
+		
+		faceUp = true;
+	}
+	
+	public Rank getRank()
+	{
+		return rank;
+	}
+	
+	public Suit getSuit()
+	{
+		return suit;
+	}
+	
+	public boolean isFaceUp()
+	{
+		return faceUp;
+	}
+	
+	public boolean isBlack()
+	{
+		return isBlack;
+	}
+	
+	public void setFaceUp(boolean faceUp)
+	{
+		this.faceUp = faceUp;
+	}
+	
+	
+}
