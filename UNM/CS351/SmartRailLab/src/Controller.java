afa5cd5a7e7438dcd50b559241c95bff0bd8394c
Anthony Galczak <wgalczak@gmail.com>
2017-10-17 16:29:03 -0600

    Atomic variable example

--- UNM/CS351/SmartRailLab/src/Controller.java
+++ UNM/CS351/SmartRailLab/src/Controller.java
@@ -1,4 +1,19 @@
+import javafx.fxml.FXML;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Controller
 {
+    @FXML
+    private void initialize()
+    {
+        // Example of atomic variables
+        AtomicInteger ai = new AtomicInteger(0);
+
+        System.out.println(ai);
+    }
+
+    public Controller() { }
+
 
 }
