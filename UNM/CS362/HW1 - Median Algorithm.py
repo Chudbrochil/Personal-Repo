6e5079218359b70cb6d340fd1864d64b071eb5b5
Anthony Galczak <wgalczak@gmail.com>
2018-01-26 18:09:53 -0700

    Got a partially working momSearch working. Not counting all comparisons yet.

--- UNM/CS362/HW1 - Median Algorithm.py
+++ UNM/CS362/HW1 - Median Algorithm.py
@@ -2,22 +2,25 @@ import random
 
 Verbosity = 1 # 0 for none, 1 for minimal, 2 for verbose
 
-# TODO: Write a pythonic shuffle method
 
+# TODO LIST:
+# The comparisons count done in momSearch is wrong. I need to also be counting the comparisons being done
+# when getting medians of blocks and then the median of medians.
+#
+# Generate stats for:
+#     How often quickSearch is "faster" than momSearch()
+#     Average Ratio of QS/MoM
+#     (DONE)Avg Cost for QS
+#     10th percentile compare
+#     90th percentile compare
+#
+# Ask about formal definition of a comparison, also some advice on implementing this in momsearch would help
+#
+# Second half of quickSearch and momSearch looks almost exactly the same...
 
-# shuffle
-# Takes in an array and returns it shuffled.
-# NOTE: This is not pythonic, but it was requested to manually shuffle elements
-def shuffle(array):
-    shuffledArray = []
-    baseArray = array[:]
-    while baseArray:
-        randomElement = baseArray.pop(random.randrange(len(baseArray)))
-        shuffledArray.append(randomElement)
-    return shuffledArray
 
 # quickSearch
-# Takes an array and calculates using the "quickSearch" algorithm the i'th
+# Takes a random array and calculates using the "quickSearch" algorithm the i'th
 # smallest element from the sorted array.
 # Returns the element found to be the i'th smallest element
 def quickSearch(array, index):
@@ -46,34 +49,92 @@ def quickSearch(array, index):
     elif index > len(leftSide) + 1:
         return quickSearch(rightSide, index - (len(leftSide) + 1))
 
-
+# momSearch
+# Takes a random array and calculates using the deterministic "median-of-medians" algorithm
+# the i'th smallest element from the sorted array.
+# blockSize - The size of the chunk we are chopping our list into
+# Returns the element found to be the i'th smallest element
 def momSearch(array, index, blockSize):
-    # TODO: Not accounting for if we have modulus left over in the blocks
+    leftSide = []
+    rightSide = []
+
+    # Chopping our array into blocks of blockSize (usually size 5)
     choppableArray = array[:]
     blockList = []
     blockOfElements = []
     while choppableArray:
-        if len(blockOfElements) == blockSize:
+
+        # If we are on our last chop of blockSize size of lower, then just add these into a block and stop
+        if(len(choppableArray) <= blockSize and len(blockOfElements) == 0):
+            blockList.append(choppableArray)
+            break
+        elif len(blockOfElements) == blockSize:
             blockList.append(blockOfElements)
             blockOfElements = []
         else:
             blockOfElements.append(choppableArray.pop())
 
 
+
+
+    # TODO: Shelving this manual sort for now... going to use python's sorted()
+    # for block in blockList:
+    #     # "Sort" the list and find the median of each block and add it to listOfMedians
+    #
+    #     # TODO: ONLY DOING THIS FOR 5 OR LESS FOR NOW
+    #     #if len(block) <= blockSize:
+    #         # Breaking into pairs
+    #         for x in range(0, len(block), 2):
+    #             print("Blocksize: %d Index: %d" % (len(block), x))
+    #             if x+1 < len(block) and block[x] > block[x+1]:
+    #                 block[x], block[x+1] = block[x+1], block[x] # in place swap
+
     # Find median in each block of 5...
+    listOfMedians = []
+    for block in blockList:
+        # TODO: I need to break this sort apart and count comparisons in here....
+        median = sorted(block)[len(block)/2]
+        listOfMedians.append(median)
 
-    # Find median of these medians...
+    if Verbosity > 1:
+        print("Full array: %s" % array[:])
+        print("List of Medians: %s" % listOfMedians[:])
 
-    # Use this as pivot, see what's above and what's below, etc.
+    # Inspired by https://brilliant.org/wiki/median-finding-algorithm/
+
+    if len(listOfMedians) <= blockSize:
+        medianOfMedians = sorted(listOfMedians)[len(listOfMedians)/2]
+    else:
+        medianOfMedians = momSearch(listOfMedians, len(listOfMedians)/2, blockSize)
+
+    for element in array:
+        if element < medianOfMedians:
+            momSearch.counter += 1
+            leftSide.append(element)
+        elif element > medianOfMedians:
+            momSearch.counter += 1
+            rightSide.append(element)
+
+    if index <= len(leftSide):
+        return momSearch(leftSide, index, blockSize)
+    elif index == len(leftSide) + 1:
+        return medianOfMedians
+    elif index > len(leftSide) + 1:
+        return momSearch(rightSide, index - (len(leftSide) + 1) , blockSize)
 
 
+# shuffle
+# Takes in an array and returns it shuffled.
+# NOTE: This is not pythonic, but it was requested to manually shuffle the elements
+def shuffle(array):
+    shuffledArray = []
+    baseArray = array[:]
+    while baseArray:
+        randomElement = baseArray.pop(random.randrange(len(baseArray)))
+        shuffledArray.append(randomElement)
+    return shuffledArray
 
-    print(blockList)
 
-
-
-
-# HELPER METHODS FOR ORGANIZING MAIN FUNCTIONALITY
 # createListOfRandomLists
 # Creates howManyLists(int) of lists containing random values from firstListValue(int) to lastListValue(int)
 def createListOfRandomLists(firstListValue, lastListValue, howManyLists):
@@ -114,25 +175,40 @@ def normalizedQuickSearchRun(listOfShuffledLists, iterations, index):
 
 
 def momSearchRun(listOfShuffledLists, index, blockSize):
+
+    if Verbosity > 0:
+        print("Running median-of-median search on %d lists looking for %d'th smallest element with block size of %d."
+              % (len(listOfShuffledLists), index, blockSize))
+
+        momSearchComparisons = []
     for list in listOfShuffledLists:
+        momSearch.counter = 0
         momSearch(list, index, blockSize)
+        momSearchComparisons.append(momSearch.counter)
+
+    return momSearchComparisons
 
 
-def generateStatistics(quickSearchComparisons):
+def generateStatistics(quickSearchComparisons, momSearchComparisons):
     # Generate stats per list
-    listIndex = 1
-    sumOfAvgs = 0
+    listIndex = 0
+    sumOfQSAvgs = 0
+
+    if len(quickSearchComparisons) != len(momSearchComparisons):
+        raise Exception("A different amount of lists were ran for quick-search and median-of-medians. Crashing...")
 
     for list in quickSearchComparisons:
         # sum(list, 0.0) makes this list into a floating point list, slick way to get more precise values without python 3
         avgOfList = sum(list, 0.0) / len(list)
-        sumOfAvgs += avgOfList
+        sumOfQSAvgs += avgOfList
 
-        if Verbosity > 1:
-            print("List#%d - Comparisons Max: %d Min: %d Avg: %0.2f" % (listIndex, max(list), min(list), avgOfList))
+        if Verbosity > 0:
+            print("#%d Comparisons Quick-Search Max: %d Min: %d Avg: %0.2f MoM-Search: %d"
+                  % (listIndex, max(list), min(list), avgOfList, momSearchComparisons[listIndex]))
             listIndex += 1
 
-    quickSearchAvg = sumOfAvgs / len(quickSearchComparisons)
+
+    quickSearchAvg = sumOfQSAvgs / len(quickSearchComparisons)
 
     if Verbosity > 0:
         print("Average comparisons of all quick search runs on all %d lists: %0.2f" % (len(quickSearchComparisons), quickSearchAvg))
@@ -157,17 +233,13 @@ def main():
     # List of all the lists of comparisons taken by quick search to find the given value
     quickSearchComparisons = normalizedQuickSearchRun(listOfShuffledLists, iterations, indexToFind)
 
+    # List of how many comparisons each list took to process with mom search, only 1 iteration per list
+    # as momSearch is deterministic
     momSearchComparisons = momSearchRun(listOfShuffledLists, indexToFind, blockSize)
 
-    generateStatistics(quickSearchComparisons)
+
+    generateStatistics(quickSearchComparisons, momSearchComparisons)
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
 
