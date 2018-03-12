0bcbf8d5999af8506f4500a0ca5789b7a5abd46d
Anthony Galczak <WGalczak@gmail.com>
2018-03-11 23:24:16 -0600

    Working 2.6.4 and 2.6.5, but it is using a flawed changeRadix

--- UNM/CS357/hw2.hs
+++ UNM/CS357/hw2.hs
@@ -14,11 +14,11 @@ collatz xs = fst (findMax (collatzNums xs) (0,0))
 findMax :: [(Int, Integer)] -> (Int, Integer) -> (Int, Integer)
 findMax [] max = max
 findMax (x:xs) max -- = if snd x >= snd max then findMax xs x else findMax xs max
-	-- If # of terms is higher, set max to x
-	| snd x > snd max = findMax xs x
-	-- If # of terms is equal, set max to x if the num itself is higher
-	| (snd x >= snd max) && (fst x > fst max) = findMax xs x
-	| otherwise = findMax xs max
+    -- If # of terms is higher, set max to x
+    | snd x > snd max = findMax xs x
+    -- If # of terms is equal, set max to x if the num itself is higher
+    | (snd x >= snd max) && (fst x > fst max) = findMax xs x
+    | otherwise = findMax xs max
 
 -- Returns a tuple of (num, collatzHelper num)
 collatzNums :: [Int] -> [(Int, Integer)]
@@ -29,8 +29,8 @@ collatzNums (x:xs) = (x, collatzHelper x) : collatzNums xs
 collatzHelper :: Int -> Integer
 collatzHelper 1 = 1
 collatzHelper n
-	| n `mod` 2 == 0 = 1 + (collatzHelper (n `div` 2))
-	| otherwise = 1 + (collatzHelper (n*3 + 1))
+    | n `mod` 2 == 0 = 1 + (collatzHelper (n `div` 2))
+    | otherwise = 1 + (collatzHelper (n*3 + 1))
 
 --2.2
 haskellFileNames :: [String] -> [String]
@@ -70,11 +70,7 @@ example = (10, [1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0])
 
 --2.6 1
 makeLongInt :: Integer -> Int -> Numeral
-makeLongInt n r = (r, reverse(fixInts (getList n r)))
-
--- TODO: Eliminate this method
-fixInts :: [Integer] -> [Int]
-fixInts xs = map fromIntegral xs
+makeLongInt n r = (r, reverse(map fromIntegral (getList n r)))
 
 getList :: Integer -> Int -> [Integer]
 getList 0 r = []
@@ -87,11 +83,11 @@ evaluateLongInt :: Numeral -> Integer
 evaluateLongInt (base, nums) = toInteger(foldl ( (+) . (*base) ) 0 nums)
 
 --2.6 3
--- TODO: Remove integer arithmetic
 -- First, make an int out our given list of ints.
 -- Second, make a new list of ints with our new base
 -- Reverse that and put it in a tuple with the new base
--- NOTE: THIS IS ALL DONE WITH JUST INT MATH, NOT INTEGER
+-- 
+-- All operations are done with strictly Int arithmetic
 changeRadixLongInt :: Numeral -> Int -> Numeral 
 changeRadixLongInt (r1, ds1) r2 = (r2, reverse (newBaseList r2 (makeInt (r1, ds1))))
 
@@ -106,17 +102,42 @@ newBaseList r num = num `mod` r : newBaseList r (num `div` r)
 
 
 --2.6 4
--- TODO: Remove integer arithmetic
+-- This is loosely based on the specification that was given to us. I have removed any semblance of Integer arithmetic via
+-- (+) or in internal methods. All math in the following 3 methods is done strictly in Int arithmetic.
 addLongInts :: Numeral -> Numeral -> Numeral
 addLongInts (r1, ds1) (r2, ds2)
-	| r1 == r2 = makeLongInt (evaluateLongInt (r1, ds1) + evaluateLongInt (r2, ds2)) r1
-	| r1 < r2 = addLongInts (changeRadixLongInt (r1, ds1) r2) (r2, ds2)
-	| r1 > r2 = addLongInts (r1, ds1) (changeRadixLongInt (r2, ds2) r1)
+    | r1 == r2 = (r1, (reverse (addTwoSameBase r1 (reverseAndFix (length ds2) ds1) (reverseAndFix (length ds1) ds2) 0)))
+    | r1 < r2 = addLongInts (changeRadixLongInt (r1, ds1) r2) (r2, ds2)
+    | r1 > r2 = addLongInts (r1, ds1) (changeRadixLongInt (r2, ds2) r1)
+
+-- Normalizes the lists against eachother. The function (addTwoSameBase) that the lists of ints will be passed into is
+-- rather fragile so it needs the nums in opposite order and of same list length
+reverseAndFix :: Int -> [Int] -> [Int]
+reverseAndFix otherLen nums = reverse ((take (otherLen - (length nums)) (repeat 0)) ++ nums)
+
+-- Adds two lists of ints together
+-- If the addition will cause overflow of the given radix, then set the carry bit to 1
+addTwoSameBase :: Int -> [Int] -> [Int] -> Int -> [Int]
+addTwoSameBase r _ [] carry = if carry == 0 then [] else [carry]
+addTwoSameBase r [] _ carry = if carry == 0 then [] else [carry]
+addTwoSameBase r (x:xs) (y:ys) carry = if sum >= r then (sum `mod` r) : addTwoSameBase r xs ys 1
+                       else sum : addTwoSameBase r xs ys 0
+                       where sum = x + y + carry
 
 --2.6 5
--- TODO: Remove integer arithmetic
+--TODO: Put some comments here. This is almost a carbon copy of 2.6.4, I need to fix changeRadix
 mulLongInts :: Numeral -> Numeral -> Numeral
 mulLongInts (r1, ds1) (r2, ds2)
-	| r1 == r2 = makeLongInt (evaluateLongInt (r1, ds1) * evaluateLongInt (r2, ds2)) r1
-	| r1 < r2 = mulLongInts (changeRadixLongInt (r1, ds1) r2) (r2, ds2) 
-	| r1 > r2 = mulLongInts (r1, ds1) (changeRadixLongInt (r2, ds2) r1)
+    | r1 == r2 = (r1, (reverse (multTwoSameBase r1 (reverseAndFix (length ds2) ds1) (reverseAndFix (length ds1) ds2) 0)))
+    | r1 < r2 = mulLongInts (changeRadixLongInt (r1, ds1) r2) (r2, ds2) 
+    | r1 > r2 = mulLongInts (r1, ds1) (changeRadixLongInt (r2, ds2) r1)
+
+
+multTwoSameBase :: Int -> [Int] -> [Int] -> Int -> [Int]
+multTwoSameBase r _ [] carry = if carry == 0 then [] else [carry]
+multTwoSameBase r [] _ carry = if carry == 0 then [] else [carry]
+multTwoSameBase r (x:xs) (y:ys) carry = if sum >= r then (sum `mod` r) : multTwoSameBase r xs ys 1
+                       else sum : addTwoSameBase r xs ys 0
+                       where sum = (x * y) + carry
+
+
