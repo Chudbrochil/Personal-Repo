176c704f13c398e2812830cb6cd175fb0111efbd
Chudbrochil <WGalczak@gmail.com>
2018-03-27 19:02:06 -0600

    changes to q1, q1-q4 should be done. working on 5-7 now.

--- UNM/CS357/hw3.hs
+++ UNM/CS357/hw3.hs
@@ -1,3 +1,4 @@
+
 {-
 Anthony Galczak
 agalczak@unm.edu
@@ -8,11 +9,12 @@ import Data.List
 --3.1
 --Ex.4 on pg.109 of programming in haskell book
 balance :: [a] -> Tree a
-balance xs = undefined
+--balance [] = Leaf -- TODO: Is this case right? What should this be?
+balance [x] = Leaf x
+balance xs = Node (balance leftHalf) (balance rightHalf)
+	where (leftHalf, rightHalf) = splitHalf xs
 
-
-
-data Tree a = Leaf a | Node (Tree a) (Tree a)
+data Tree a = Leaf a | Node (Tree a) (Tree a) deriving (Eq, Show)
 
 splitHalf :: [a] -> ([a],[a])
 splitHalf xs = splitAt (length xs `div` 2) xs
@@ -33,14 +35,13 @@ prime n = factors n == [1,n]
 primes :: Int -> [Int]
 primes n = [x | x <- [2..n], prime x]
 
-
 -- Gives a list of tuples each of which correspond to a prime+prime=n where
 -- the tuple is (prime, prime)
--- TODO: Add case where if n isn't even or over 2 then return empty list (or something else...)
--- TODO: Do I need to have (3,89) and (89,3) or should I remove these?
+-- x + y == n makes sure it equals the number n, x <= y verifies we only get 1 permutation
+-- the if-else check makes sure that the number is even and >2
 goldbach :: Int -> [(Int, Int)]
-goldbach n = [(x,y) | x <- primes n, y <- primes n, x + y == n]
-
+goldbach n = if n > 2 && n `mod` 2 == 0 then gold' else []
+	where gold' = [(x,y) | x <- primes n, y <- primes n, x + y == n, x <= y]
 
 
 --3.3
@@ -50,6 +51,41 @@ goldbach n = [(x,y) | x <- primes n, y <- primes n, x + y == n]
 -- In this case it is (tail . tail . tail . tail . id) applied to "ABCDEFGH"
 church :: Int -> (c -> c) -> c -> c
 church n = (\f -> foldr (.) id (replicate n f)) 
+
+
+--3.4 Recursive Functions Over Lists (10pts)
+type Set = [Int]
+
+powerset :: [Int] -> [[Int]]
+powerset = subsequences
+
+--3.5 Lists And Strings (10pts)
+example :: [[(Double, Double)]]
+example = [[(100.0,100.0),(100.0,200.0),(200.0,100.0)],
+  [(150.0,150.0),(150.0,200.0),(200.0,200.0),(200.0,150.0)]]
+
+makeCommand :: [[(Double, Double)]] -> String
+makeCommand = undefined
+
+--3.6 Trees (25pts)
+{-data T = Leaf | Node T T deriving (Eq, Show)
+
+data P = GoLeft P | GoRight P | This deriving (Eq, Show)
+
+allpaths :: T -> [P]
+allpaths = undefined
+-}
+--3.7 Logic (25pts)
+type Expr = [[Int]]
+
+eval :: (Int -> Bool) -> Expr -> Bool
+eval = undefined
+
+satisfiable :: Expr -> Bool
+satisfiable = undefined
+
+
+
 
 
 
