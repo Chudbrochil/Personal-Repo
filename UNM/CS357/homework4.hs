f2296d4ddde80bbf621568cdebef0b7c369a5e64
Anthony Galczak <WGalczak@gmail.com>
2018-04-16 19:51:56 -0600

    Changed Board to proper declaration of [Field], Now need to get some IO going.

--- UNM/CS357/homework4.hs
+++ UNM/CS357/homework4.hs
@@ -67,11 +67,11 @@ fileisort fn1 fn2 = writeFile fn2 (L.intercalate "\n" (isort (lines fn1)))
 -- Instead of X and O, we use (R)ed and (G)reen
 data Field = B | R | G
              deriving (Eq, Ord, Show)
-type Board = [[Field]] -- TODO: This has to be changed back to just a [Field]
+type Board = [Field]
 
-type Grid = [[Player]]
-type Player = O | B | X
-							deriving (Eq, Ord, Show)
+--type Grid = [[Player]]
+--type Player = O | B | X
+--							deriving (Eq, Ord, Show)
 
 strategyForRed :: Board -> Int
 strategyForRed = undefined
@@ -91,50 +91,47 @@ next B = B
 
 
 empty :: Board
-empty = replicate size (replicate size B)
+empty = replicate (size * size) B
 
 full :: Board -> Bool
-full = all (/= B) . concat
+full = all (/= B)
 
 turn :: Board -> Field
 turn g = if os <= xs then G else R
 	where
-		os = length (filter (== G) ps)
-		xs = length (filter (== R) ps)
-		ps = concat g
+		os = length (filter (== G) g)
+		xs = length (filter (== R) g)
 
 wins :: Field -> Board -> Bool
 wins p g = any line (rows ++ cols ++ dias)
 	where
 		line = all (== p)
-		rows = g
-		cols = L.transpose g
-		dias = [diag g, diag (map reverse g)]
+		rows = [[g !! (offset + (n * size)) | offset <- [0..size-1]  ] | n <- [0..size-1]]
+		-- Made something that limits this to 3x3 here... -1,0,1... fix later.
+		cols = [[g !! ((n * size) + offset) | n <- [0..size-1]] | offset <- [0..size-1]]
+		dias = [diag g, diag (reverse g)]
 
 
 diag :: Board -> [Field]
-diag g = [g !! n !! n | n <- [0..size-1]]
+diag g = [g !! ((n * size) + n) | n <- [0..size-1]]
 
 
 won :: Board -> Bool
 won g = wins G g || wins R g
 
 
-
-
-
 valid :: Board -> Int -> Bool
-valid g i = 0 <= i && i < size^2 && concat g !! i == B
+valid g i = 0 <= i && i < size^2 && g !! i == B
 
 
 move :: Board -> Int -> Field -> [Board]
 move g i p =
-	if valid g i then [chop size (xs ++ [p] ++ ys)] else []
-	where (xs, B:ys) = splitAt i (concat g)
+	if valid g i then [xs ++ [p] ++ ys] else []
+	where (xs, B:ys) = splitAt i g
 
-chop :: Int -> [a] -> [[a]]
-chop n [] = []
-chop n xs = take n xs : chop n (drop n xs)
+--chop :: Int -> [a] -> [[a]]
+--chop n [] = []
+--chop n xs = take n xs : chop n (drop n xs)
 
 data Tree a = Node a [Tree a]
 							deriving Show
@@ -179,10 +176,10 @@ bestmove g p = head [g' | Node (g', p') _ <- ts, p' == best]
 -- Grid => Board
 -- Player => Field
 
-putBoard :: Board -> IO()
-putBoard =
-	putStrLn . unlines . concat . interleave bar . map showRow
-	where bar = [replicate (size*4-1) '-']
+--putBoard :: Board -> IO()
+--putBoard =
+--	putStrLn . unlines . concat . interleave bar . map showRow
+--	where bar = [replicate (size*4-1) '-']
 
 --showRow ::
 
