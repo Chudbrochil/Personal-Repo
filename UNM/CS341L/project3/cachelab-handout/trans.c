d2c428d29c297b1e02854b36c49dbd8d9ea483a2
Anthony Galczak <WGalczak@gmail.com>
2018-04-28 16:15:58 -0600

    Worked on the matrix transpose. Got to 15.5 / 26 points so far. Will need to work on 64x64 and 61x67.

--- UNM/CS341L/project3/cachelab-handout/trans.c
+++ UNM/CS341L/project3/cachelab-handout/trans.c
@@ -11,6 +11,11 @@
 #include "cachelab.h"
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
+void trans(int M, int N, int A[N][M], int B[M][N]);
+void trans32(int M, int N, int A[N][M], int B[M][N]);
+void trans64(int M, int N, int A[N][M], int B[M][N]);
+void trans67(int M, int N, int A[N][M], int B[M][N]);
+
 
 /* 
  * transpose_submit - This is the solution transpose function that you
@@ -22,6 +27,18 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+  if(N == 32)
+  {
+    trans32(M, N, A, B);
+  }
+  else if(N == 64)
+  {
+    trans64(M, N, A, B);
+  }
+  else
+  {
+    trans67(M, N, A, B);
+  }
 }
 
 /* 
@@ -43,8 +60,166 @@ void trans(int M, int N, int A[N][M], int B[M][N])
             B[j][i] = tmp;
         }
     }    
+}
+
+
+char trans32_desc[] = "Transpose for 32x32 matrix.";
+void trans32(int M, int N, int A[N][M], int B[M][N])
+{
+  int blockSize = 8;
+  int i, j, k, l;
+
+  int diagElement;
+  int diagIndex;
+
+  // Outer loop for gathering each block. 8x8 for the 32x32 matrix
+  for(i = 0; i < M; i += blockSize)
+  {
+    for(j = 0; j < N; j += blockSize)
+    {
+
+      // Inner loop for the transpose
+      for(k = j; k < j + blockSize; ++k)
+      {
+        for(l = i; l < i + blockSize; ++l)
+        {
+          if(k != l)
+          {
+            B[l][k] = A[k][l];
+          }
+          // Assign the diagonal element and index for later use
+          else
+          {
+            diagElement = A[k][l];
+            diagIndex = l;
+          }
+          
+        }
+
+        // We can save some misses on the diagonals
+        if(i == j)
+        {
+          B[diagIndex][diagIndex] = diagElement;
+        }
+      }
+    }
+  }
+}
+
+
+char trans64_desc[] = "Transpose for 64x64 matrix.";
+void trans64(int M, int N, int A[N][M], int B[M][N])
+{
+
+  int blockSize = 4;
+  int i, j, k, l;
+
+  int diagElement;
+  int diagIndex;
+
+  // Outer loop for gathering each block. 4x4 for 64 matrix
+  for(i = 0; i < M; i += blockSize)
+  {
+    for(j = 0; j < N; j += blockSize)
+    {
+
+      // Inner loop for the transpose
+      for(k = j; k < j + blockSize; ++k)
+      {
+        for(l = i; l < i + blockSize; ++l)
+        {
+          if(k != l)
+          {
+            B[l][k] = A[k][l];
+          }
+          else
+          {
+            diagElement = A[k][l];
+            diagIndex = l;
+          }
+          
+        }
+
+        // We can save some misses on the diagonals
+        if(i == j)
+        {
+          B[diagIndex][diagIndex] = diagElement;
+        }
+      }
+    }
+  }
+}
+
+
+
+char trans67_desc[] = "Transpose for 61x67 matrix.";
+void trans67(int M, int N, int A[N][M], int B[M][N])
+{
+
+  int blockSize = 4;
+  int i, j, k, l;
+
+  int diagElement;
+  int diagIndex;
+
+  int optimizedBound = 60;
+
+  // Outer loop for gathering each block. 4x4 upto 60x60 for the 61x67 matrix
+  for(i = 0; i < optimizedBound; i += blockSize)
+  {
+    for(j = 0; j < optimizedBound; j += blockSize)
+    {
+
+      // Inner loop for the transpose
+      for(k = j; k < j + blockSize; ++k)
+      {
+        for(l = i; l < i + blockSize; ++l)
+        {
+          //B[l][k] = A[k][l];
+          
+          if(k != l)
+          {
+            B[l][k] = A[k][l];
+          }
+          else
+          {
+            diagElement = A[k][l];
+            diagIndex = l;
+          }
+          
+        }
+
+        // We can save some misses on the diagonals
+        if(i == j)
+        {
+          B[diagIndex][diagIndex] = diagElement;
+        }
+      }
+    }
+  }
+
+  // This part is totally un-optimized, I'm just finishing the transpose
+  // Getting the right most ones we left out
+  for(i = optimizedBound; i < N; ++i)
+  {
+    for(j = 0; j < M; ++j)
+    {
+      A[i][j] = B[j][i];
+    }
+  }
+  // Getting the bottom most ones we left out
+  for(i = 0; i < optimizedBound; ++i)
+  {
+    for(j = optimizedBound; j < M; ++j)
+    {
+      A[i][j] = B[j][i];
+    }
+  }
 
 }
+
+
+
 
 /*
  * registerFunctions - This function registers your transpose
@@ -61,6 +236,9 @@ void registerFunctions()
     /* Register any additional transpose functions */
     registerTransFunction(trans, trans_desc); 
 
+    registerTransFunction(trans32, trans32_desc);
+    registerTransFunction(trans64, trans64_desc);
+    registerTransFunction(trans67, trans67_desc);
 }
 
 /* 
